b645f21637325f55c1b9b94f0f0f04c4cac28309
teng.li <teng.li@yhlsoft.com>
2022-12-05 18:12:04 +0800

    add trie tree

--- yoo/router.go
+++ yoo/router.go
@@ -3,29 +3,56 @@ package yoo
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
-  handlers map[string]HandlerFunc
+	roots    map[string]*node
+	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
-  return &router{ handlers: make(map[string]HandlerFunc)}
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
 }
 
-func (r *router) AddRoute(method string, pattern string, handler HandlerFunc)  {
-  log.Printf("Route %4s - %s", method, pattern)
+func parsePattern(pattern string) []string {
+	splits := strings.Split(pattern, "/")
+	parts := make([]string, 0)
 
-  key := method + "-" + pattern
-  r.handlers[key] = handler
+	for _, part := range splits {
+		if part != "" {
+			parts = append(parts, part)
+
+			if part[0] == '*' {
+				break
+			}
+		}
+	}
+
+	return parts
+}
+
+func (r *router) AddRoute(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+
+  parts := parsePattern(pattern)
+  if _, ok := r.roots[method]; !ok {
+    r.roots[method] = &node{}
+  }
+  r.roots[method].insert(pattern, parts, 0)
+
+	key := method + "-" + pattern
+	r.handlers[key] = handler
 }
 
 func (r *router) Handle(c *Context) {
-  router_key := c.Req.Method + "-" + c.Req.URL.Path
-  if handler, ok := r.handlers[router_key]; ok {
-    handler(c)
-  } else {
-    c.String(http.StatusNotFound, "404 Page not found")
-  }
+	router_key := c.Req.Method + "-" + c.Req.URL.Path
+	if handler, ok := r.handlers[router_key]; ok {
+		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 Page not found")
+	}
 }
-
